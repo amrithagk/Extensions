a8c71dc634a91133a25cd31766aad1d394ad4ed3
Amritha <101631715+amrithagk@users.noreply.github.com>
2023-07-25 22:59:48 +0530

    Updated functionality

--- Tab list/tabs.js
+++ Tab list/tabs.js
@@ -1,12 +1,29 @@
 function display(arr)
 {
     const ol = document.createElement("ol");
+
     arr.map((tab, index)=>{
+
         let li = document.createElement("li");
         li.setAttribute("id", "tab"+index.toString());
+
         let anchor = document.createElement("a");
+        anchor.setAttribute('class', "tab-link")
         anchor.setAttribute('href', tab.url);
         anchor.innerText = tab.title;
+
+        anchor.addEventListener("click", function(event){
+            
+            let tag = event.target;
+            chrome.tabs.query({url:tag.href}, function(tabs){
+
+                if (tabs && tabs.length > 0)
+                    chrome.tabs.update(tabs[0].id, {active : true})
+
+            })
+            //window.open(tag.href, "_blank")
+        })
+
         li.appendChild(anchor);
         ol.appendChild(li);
     })
@@ -35,4 +52,4 @@ function getTabList()
 
 }
 
-document.addEventListener("DOMContentLoaded", getTabList)
+document.addEventListener("DOMContentLoaded", getTabList);
